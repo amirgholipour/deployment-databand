2c4d895f194bad81e95e2d0529f39d83d333d979
Angel Gonzalez <angelito@de.ibm.com>
2023-03-13 11:06:28 +0100

    Revert "delete"
    This reverts commit fd2c105a829bd1e250967b6427c77a612ae49197.

---
+++ dags/sql/motogp_create_table.sql
@@ -0,0 +1,9 @@
+create table 
+if not exists
+motogp(circuit varchar(40), 
+      class char(6),
+      constructor varchar(40),
+      country char(2),
+      rider varchar(40),
+      season integer)
+;
