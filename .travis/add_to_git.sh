c9e9e626cbf866fa65cfb836ec6d524ddb5a23b7
Angel Gonzalez <angelito@de.ibm.com>
2023-03-29 17:03:50 +0200

    Update add_to_git.sh

--- .travis/add_to_git.sh
+++ .travis/add_to_git.sh
@@ -6,7 +6,7 @@ then
   git config user.email "Travis CI"
   git checkout -q main
   git add README.docx
-  git commit --allow-empty -m "Update repo [skip ci]"
+  git commit --allow-empty -m "create docx via travis"
   git remote add authenticated https://$GITHUB_TOKEN@github.ibm.com/angelito/databand-workshop.git
   git push --quiet authenticated main &>/dev/null
 else
