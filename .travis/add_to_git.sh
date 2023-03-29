5c67ce412d1270666842155b7a0dc1bb2f5ebc7e
Angel Gonzalez <angelito@de.ibm.com>
2023-03-29 16:44:13 +0200

    Update add_to_git.sh

--- .travis/add_to_git.sh
+++ .travis/add_to_git.sh
@@ -5,10 +5,9 @@ then
   git config user.name "Travis CI"
   git config user.email "Travis CI"
   git checkout -q main
-  git add article/README.pdf
-  git add article/README.docx
+  git add README.pdf
   git commit --allow-empty -m "Update repo [skip ci]"
-  git remote add authenticated https://$GITHUB_TOKEN@github.ibm.com/germany-lab/qiskit-article-linux-magazine.git
+  git remote add authenticated https://$GITHUB_TOKEN@github.ibm.com/angelito/databand-workshop.git
   git push --quiet authenticated main &>/dev/null
 else
   echo Github token not available
