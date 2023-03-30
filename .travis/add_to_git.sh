866eb35d7618776e03d6d0e463777233b9e4f112
Angel Gonzalez <angelito@de.ibm.com>
2023-03-30 12:06:49 +0200

    [skip travis]

--- .travis/add_to_git.sh
+++ .travis/add_to_git.sh
@@ -11,7 +11,7 @@ then
    git add $i
   done
 
-  git commit --allow-empty -m "create docx [skip ci] "
+  git commit --allow-empty -m "[skip travis] create documents via travis "
   git remote add authenticated https://$GITHUB_TOKEN@github.ibm.com/angelito/databand-workshop.git
   git push --quiet authenticated main &>/dev/null
 else
