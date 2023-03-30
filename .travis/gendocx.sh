403bbec3ab755536d35b27e0c529a319bb0347e1
Angel Gonzalez <angelito@de.ibm.com>
2023-03-30 10:03:36 +0200

    again

--- .travis/gendocx.sh
+++ .travis/gendocx.sh
@@ -1,15 +1,11 @@
 #!/bin/bash -e
 
-cd jupyter
-for i in *
+for i in jupyter/*.ipynb
 do
    myipynb=$(basename $i)
    mydocx=$(basename $i .ipynb).docx
 
-   docker run --rm --volume "$(pwd):/data" --user `id -u`:`id -g` pandoc/latex $myipynb -o ../docs/$mydocx
-
-   # docker run --rm --volume "$(pwd)/jupyter:/data" --volume "$(pwd)/pictures:/pictures" --volume "$(pwd)/docs:/docs" --user `id -u`:`id -g` pandoc/latex $myipynb -o ../docs/$mydocx
+   docker run --rm --volume "$(pwd):/data" --volume "$(pwd)/pictures:/pictures" --volume "$(pwd)/docs:/docs" --user `id -u`:`id -g` pandoc/latex $myipynb -o ../docs/$mydocx
 done
 
-cd ..
 docker run --rm --volume "$(pwd):/data" --user $(id -u):$(id -g) pandoc/latex README.md -o ./docs/README.docx
